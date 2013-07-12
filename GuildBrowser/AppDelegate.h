d7d21ee1cb4152fff52e130cd82bfb20f56b4803
Chandan Kumar <bluekanishk@gmail.com>
2013-07-12 14:35:57 +0530

    Initial commit

--- GuildBrowser/AppDelegate.h
+++ GuildBrowser/AppDelegate.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+// This comment is added by Chandan Kumar
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
